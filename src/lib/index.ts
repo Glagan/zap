d5f9b2e4ac97486f32a9d5bb7fd478e1ad85ed93
Glagan <Glagan@users.noreply.github.com>
2022-09-13 01:47:59 +0200

    Update exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,5 @@
 import ZapContainer from './components/ZapContainer.svelte';
-import Notification from './components/Notification.svelte';
 import zapOptions from './stores/zapOptions';
-import wrappers from './stores/wrappers';
 import zap from './stores/zap';
-import deepAssign from './utils/deepAssign';
 
-export { zap as default, zap, zapOptions, wrappers, Notification, ZapContainer, deepAssign };
+export { zap as default, zap, zapOptions, ZapContainer };
